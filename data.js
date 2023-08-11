0b59637719539df51f74748776f4508db8737fc6
Aayush2699 <142033543+Aayush2699@users.noreply.github.com>
2023-08-11 21:13:00 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+let songs = [
+    {
+        name: 'Perfect',
+        path: 'music\song 1.mp3',
+        artist: 'Ed Sheeran',
+        cover: 'images\cover 1.png',
+    },
+    {
+        name: 'Dil Ibaadat',
+        path: 'music\song 2.mp3',
+        artist: 'Pritam,KK',
+        cover: 'images\cover 2.png'
+    },
+    {
+        name: 'Phir Aur Kya Chahiye',
+        path: 'music\song 3.mp3',
+        artist: 'Arijit Singh',
+        cover: 'images\cover 3.png'
+    },
+    {
+        name: 'Hawa Banke',
+        path: 'music\song 4.mp3',
+        artist: 'Darshan Rawal',
+        cover: 'images\cover 4.png'
+    
+    },
+    {
+        name: 'Abhi Mujh Mein Kahin',
+        path: 'music\song 5.mp3',
+        artist: 'Sonu Nigam',
+        cover: 'images\cover 5.png'
+    }
+       ]
